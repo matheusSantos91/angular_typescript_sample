e4f32b8e965d6bfd62394799672e758a7745c851
Matheus Santos <Matheus.Santos@wexinc.com>
2025-01-10 11:02:18 -0300

    Add Singleton, dependency

--- app.module.ts
+++ app.module.ts
@@ -2,10 +2,14 @@ import { CoursesComponent } from './couses.component';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesService } from './courses.service';
 
 @NgModule({
     declarations: [
         AppComponent,
         CoursesComponent
+    ],
+    providers: [
+        CoursesService
     ]
 })
